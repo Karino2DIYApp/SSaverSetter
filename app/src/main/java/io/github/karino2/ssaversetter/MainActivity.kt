80935ee56484fbe9e34b0510cc9a60e18d4c0c9d
Kazuma Arino <hogeika2@gmail.com>
2026-06-10 12:12:10 +0900

    Pictures下を見て毎回ファイル名を変えるように変更
    同じ名前だと更新してくれないので名前を変えていく。
    ゴミがPictures下に残ってしまうのでそこは手で削除してもらう（このアプリの管理外の場所なので仕方ない）
    Pictures下は読む事は出来るがdeleteは出来ないっぽい。

--- app/src/main/java/io/github/karino2/ssaversetter/MainActivity.kt
+++ app/src/main/java/io/github/karino2/ssaversetter/MainActivity.kt
@@ -6,6 +6,7 @@ import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
+import android.provider.MediaStore
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -15,6 +16,7 @@ import androidx.core.view.WindowInsetsCompat
 import com.onyx.android.sdk.api.device.screensaver.ScreenResourceManager
 import java.io.File
 import java.io.FileOutputStream
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,6 +34,30 @@ class MainActivity : AppCompatActivity() {
                 handleSendImage(it)
             }
         }
+
+    }
+
+    // 前回と同じ名前だとインテント受け取った側で無視されてしまう。
+    // Pictures下にファイルがあるかどうかで判定しているっぽい。
+    // このファイルのwrite permissionは無いので違う名前をつけていく。
+    // 定期的に手で消してね。
+    // このファイルパスで良いかはデバイス依存かもしれないので、無理そうならデフォルトの名前を返す。
+    private fun findNextFileName() : String {
+        try
+        {
+            val prefix = "/storage/emulated/0/Pictures/SSaverSetter"
+            for(i in 0 until 100) {
+                if(!File("${prefix}-${i}.png").exists())
+                {
+                    return "SSaverSetter-${i}.png"
+                }
+            }
+            println("giveup")
+            return "SSaverSetter.png"
+        }catch(_: IOException) {
+            println("io exception")
+            return "SSaverSetter.png"
+        }
     }
 
     private fun handleSendImage(intent: Intent) {
@@ -43,7 +69,17 @@ class MainActivity : AppCompatActivity() {
                     if (bitmap != null) {
                         val picturesDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
                         if (picturesDir != null && !picturesDir.exists()) picturesDir.mkdirs()
-                        val file = File(picturesDir, "SSaverSetter.png")
+
+                        // これまで作ったファイルは削除する。基本的にはアプリprivateなエリアなので全部消して良いはずだが、
+                        // 昔の変な実装が残ったデバイスとかあったら嫌なので名前でフィルタしておく。
+                        picturesDir?.listFiles()?.forEach {
+                            if (it.name.startsWith("SSaverSetter") && it.name.endsWith(".png")) {
+                                it.delete()
+                            }
+                        }
+
+                        val file = File(picturesDir, findNextFileName())
+
                         FileOutputStream(file).use { out ->
                             bitmap.compress(Bitmap.CompressFormat.PNG, 100, out)
                         }
@@ -51,7 +87,7 @@ class MainActivity : AppCompatActivity() {
                         // Use ScreenResourceManager to set screensaver.
                         val path = file.absolutePath
                         val success = ScreenResourceManager.setScreensaver(this, path, true)
-                        
+
                         if (success) {
                             Toast.makeText(this, "Screensaver set!", Toast.LENGTH_SHORT).show()
                         } else {
